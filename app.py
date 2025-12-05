be9ff9def3ac329ae9fe604ae12f37e5a0a283e8
sri-is-coding <146610065+sri-is-coding@users.noreply.github.com>
2025-12-05 15:09:31 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/")
+def home():
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    task = request.form["task"]
+    tasks.append(task)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
